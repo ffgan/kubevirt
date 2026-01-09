150a8f50ad5d1c2a081359ed05ee58ee30daeac6
ffgan <sudoemt@gmail.com>
2026-01-09 10:38:15 +0800

    remove unused code

--- hack/builder/common.sh
+++ hack/builder/common.sh
@@ -1,14 +1,5 @@
-# DOCKER_PREFIX=${DOCKER_PREFIX:-"quay.io/kubevirt"}
-# DOCKER_IMAGE=${DOCKER_IMAGE:-"builder"}
-
-if [ "$(uname -m)" == "riscv64" ]; then
-    DOCKER_PREFIX=${DOCKER_PREFIX:-"registry.risc-vers.cn/wg-cloudcomputing"}
-    DOCKER_IMAGE=${DOCKER_IMAGE:-"kubevirt-builder"}
-else
-    DOCKER_PREFIX=${DOCKER_PREFIX:-"quay.io/kubevirt"}
-    DOCKER_IMAGE=${DOCKER_IMAGE:-"builder"}
-fi
-
+DOCKER_PREFIX=${DOCKER_PREFIX:-"quay.io/kubevirt"}
+DOCKER_IMAGE=${DOCKER_IMAGE:-"builder"}
 DOCKER_CROSS_IMAGE=${DOCKER_CROSS_IMAGE:-"builder-cross"}
 
 ARCHITECTURES=${ARCHITECTURES:-"amd64 arm64 s390x riscv64"}
